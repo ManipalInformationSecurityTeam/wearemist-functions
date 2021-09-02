f9d38cf7de9ee27b93d05e090de59486f525606f
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-09-02 22:36:46 +0530

    cors update

--- pages/api/events/index.js
+++ pages/api/events/index.js
@@ -1,7 +1,24 @@
 import dbConnect from "../../../utils/dbConnect";
 import Event from "./../../../models/Event";
 
-export default async function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+async function handler(req, res) {
     await dbConnect();
 
     const { method, body, query, headers } = req;
@@ -77,3 +94,5 @@ export default async function handler(req, res) {
         res.status(500).json({ success: false });
     }   
 }
+
+module.exports = allowCors(handler)
